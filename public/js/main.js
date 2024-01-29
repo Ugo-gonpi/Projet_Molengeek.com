974af88f79d9f443d93666befe1deea0f497f105
Ugo-gonpi <ugogonzalez50@gmail.com>
2024-01-29 12:16:12 +0100

    arborescence de base crée

---
+++ public/js/main.js
@@ -0,0 +1,12 @@
+import * as INSTANCES from "./modules/instances.js"
+
+import data from "./../json/molengeek.json" assert { type: "json" }
+
+let logo = data.molengeekLogo
+
+let img 
+
+let molenLogo = () => {
+    img = document.createElement("img")
+    
+}
