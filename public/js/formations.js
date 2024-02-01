1406c5e9365b868240b94275782367933c997433
Ugo-gonpi <ugogonzalez50@gmail.com>
2024-02-01 14:47:18 +0100

    added the testimonies videos

--- public/js/formations.js
+++ public/js/formations.js
@@ -26,5 +26,34 @@ logoFooter()
 
 //Ici on récupere les questions et les réponse
 
-let questions = data.FAQ
+// let questions = data.FAQ
 
+// let count2 = 0
+
+
+// let quesReponse = () => {
+//     for (const key in questions) {
+//         if (Object.hasOwnProperty.call(questions, key)) {
+//             const element = questions[key];
+//             let h3 = document.createElement("h3")
+//             let p = document.createElement("p")
+//             h3.innerText = element.question
+//             p.innerText = element.reponse
+//             count2 += 1
+//         }
+//     }
+// }
+
+//Ici je récupere les temoignages
+
+// let testimonials1 = data.videosTemoignage.videoI
+
+// let testimonials2 = data.videosTemoignage.videoII
+
+let temoin1 = document.querySelector(".video1")
+
+let temoin2 = document.querySelector(".video2")
+
+temoin1.src = data.videosTemoignage.videoI
+
+temoin2.src = data.videosTemoignage.videoII
